79189eccff3795fbd40367e4f86277ca848c0df0
Sparsh Goel <sparshgoelt26@gmail.com>
2023-09-06 00:38:44 +0530

    Added Api call and code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Weather Application</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [data, setData] = useState([]);
+
+  const fetchDataFromApi = async () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+    const completeURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e8f5eb4d5ac847385c220ddc60d5ed29`;
+    console.log(completeURL);
+    await fetch(completeURL)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      });
+  };
+
+  useEffect(() => {
+    fetchDataFromApi();
+  }, [latitude, longitude]);
+
+  return <div>Weather Application</div>;
+};
 
 export default App;
